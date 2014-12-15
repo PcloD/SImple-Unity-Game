8638f49ba48942c0c59f1e082e6226f272413437
Ferologics <f.hetes@gmail.com>
2014-12-15 22:00:55 +0100

    Initial commit
    push baby push

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	
+	private Vector3 input;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+//	void Update () {
+//
+//	}
+
+	void Update () {
+		input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+		if (rigidbody.velocity.magnitude < 4) {
+			rigidbody.AddForce (input * speed * 1.5f);
+
+		} else if (rigidbody.velocity.magnitude > 4){
+			rigidbody.AddForce (input * speed);
+
+		}
+	}
+}
